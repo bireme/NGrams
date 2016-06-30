36960c9da9de8953d5f9cb10a29f49e9fcbf486e
heitorbarbieri <barbieri@paho.org>
2016-06-30 10:04:48 -0300

    lucene version 6.0.1

--- indexaMEDLINE.sh
+++ indexaMEDLINE.sh
@@ -1,1 +1,1 @@
-java -cp dist/NGrams.jar:dist/lib/lucene-analyzers-common-5.3.0.jar:dist/lib/lucene-queryparser-5.3.0.jar:dist/lib/lucene-core-5.3.0.jar:dist/lib/lucene-suggest-5.3.0.jar  br.bireme.ngrams.NGrams index medline medline ./config.cfg ISO-8859-1 ./medline.txt ISO-8859-1
+java -cp dist/NGrams.jar:dist/lib/lucene-analyzers-common-6.1.0.jar:dist/lib/lucene-queryparser-6.1.0.jar:dist/lib/lucene-core-6.1.0.jar:dist/lib/lucene-suggest-6.1.0.jar  br.bireme.ngrams.NGrams index medline medline ./config.cfg ISO-8859-1 ./medline.txt ISO-8859-1
